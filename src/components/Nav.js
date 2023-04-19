02ce40dff8727a308aae53f038a4cda642cabfe0
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2023-04-19 14:22:13 +0530

    navbar fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -22,52 +22,65 @@ const Nav = () => {
   return (
     <nav className="navbar">
       <div className="container">
+        {isMobile ?
+           <div className="navbar__container">
+           <ul className="navbar__left">
+             <div className="navbar__left-logo"></div>
+           </ul>
+           {state ? (
+             <ul className="navbar__right">
+               <li onClick={() => setState(!state)}>
+                 <a href="#Banner">Home</a>
+               </li>
+ 
+               <li onClick={() => setState(!state)}>
+                 <a href="#About">About</a>
+               </li >
+               <li onClick={() => setState(!state)}>
+                 <a href="#Skills">Skills</a>
+               </li>
+               <li onClick={() => setState(!state)}>
+                 <a href="#Projects">Projects</a>
+               </li>
+ 
+               <li onClick={() => setState(!state)}>
+                 <a href="#Contact">Contact</a>
+               </li>
+             </ul>
+           ) : (
+             ""
+           )}
+         </div>
+        :
         <div className="navbar__container">
-          <ul className="navbar__left">
-            <div className="navbar__left-logo"></div>
+        <ul className="navbar__left">
+          <div className="navbar__left-logo"></div>
+        </ul>
+        {state ? (
+          <ul className="navbar__right">
+            <li>
+              <a href="#Banner">Home</a>
+            </li>
+
+            <li>
+              <a href="#About">About</a>
+            </li >
+            <li>
+              <a href="#Skills">Skills</a>
+            </li>
+            <li>
+              <a href="#Projects">Projects</a>
+            </li>
+
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
           </ul>
-          {isMobile && state ? (
-            <ul className="navbar__right">
-              <li onClick={() => setState(!state)}>
-                <a href="#Banner">Home</a>
-              </li>
-
-              <li onClick={() => setState(!state)}>
-                <a href="#About">About</a>
-              </li >
-              <li onClick={() => setState(!state)}>
-                <a href="#Skills">Skills</a>
-              </li>
-              <li onClick={() => setState(!state)}>
-                <a href="#Projects">Projects</a>
-              </li>
-
-              <li onClick={() => setState(!state)}>
-                <a href="#Contact">Contact</a>
-              </li>
-            </ul>
-          ) : (
-            <ul className="navbar__right">
-              <li onClick={() => setState(!state)}>
-                <a href="#Banner">Home</a>
-              </li>
-
-              <li onClick={() => setState(!state)}>
-                <a href="#About">About</a>
-              </li >
-              <li onClick={() => setState(!state)}>
-                <a href="#Skills">Skills</a>
-              </li>
-              <li onClick={() => setState(!state)}>
-                <a href="#Projects">Projects</a>
-              </li>
-
-              <li onClick={() => setState(!state)}>
-                <a href="#Contact">Contact</a>
-              </li>
-            </ul>
-          )}
-        </div>
+        ) : (
+          ""
+        )}
+      </div>
+         }
       </div>
       <div className="toggle" onClick={() => setState(!state)}>
         <FaBars className="fa" />
